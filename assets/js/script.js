7f828c7a79cc7e57dab97ee5ce744c496865ea80
aamandaericson <amandaericson1@hotmail.com>
2022-12-27 13:45:23 +0000

    Add function and eventlisteners for buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,8 +6,19 @@ const resultDisplay = document.getElementById('result')
 const possibleChoices = document.getElementsByClassName('button')
 let userChoice;
 
+ /*Wait for the DOM to finnish loading before start. Get the button elements by class name and add event listeners to them.
+ Display user choice */
 
 document.addEventListener("DOMContentLoaded", init());
+
+function init() {
+    for (let possibleChoice of possibleChoices) {
+        possibleChoice.addEventListener("click", function() {
+            userChoice = this.getAttribute("data-type"); 
+            userChoiceDisplay.innerHTML = userChoice;
+            });
+        };
+    };
 
 
 
