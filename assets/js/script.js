d27e25e56c34ef440032f7898e45218a5b6b4717
aamandaericson <amandaericson1@hotmail.com>
2022-12-27 14:55:08 +0000

    Add get result function for different scenarios

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,6 +20,7 @@ function init() {
             userChoice = this.getAttribute("data-type"); 
             userChoiceDisplay.innerHTML = userChoice;
             generateCpuChoice()
+            getResult()
             });
         };
     };
@@ -52,6 +53,35 @@ function generateCpuChoice() {
     cpuChoiceDisplay.innerHTML = cpuChoice;
 
 }
+
+/**
+ * Get results in different game scenarios
+ * and display round result
+ */
+function getResult() {
+
+    if (cpuChoice === userChoice) {
+        result = 'Draw!'
+    } else if (cpuChoice === 'rock' && userChoice ==='paper') {
+        result = 'Paper beats rock! You get a point!'
+    } else if (cpuChoice === 'rock' && userChoice ==='scissors') {
+        result = 'Rock beats scissors! Computer get a point!'
+    } else if (cpuChoice === 'paper' && userChoice ==='scissors') {
+        result = 'Scissors beats paper! You get a point!'
+    } else if (cpuChoice === 'paper' && userChoice ==='rock') {
+        result = 'Paper beats rock! Computer get a point!'
+    } else if (cpuChoice === 'scissors' && userChoice ==='rock') {
+        result = 'Rock beats scissors! You get a point!'
+    } else if (cpuChoice === 'scissors' && userChoice ==='paper') {
+        result = 'scissors beats paper! Computer get a point!'
+    } else {
+        alert(`Choose Rock, Paper or scissors!`);
+        throw `unidentified choice`;
+    }
+
+    resultDisplay.innerHTML = result  
+
+} 
 
 
 
