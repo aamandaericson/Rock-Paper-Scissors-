ede0c23313bd6c02a73932533c1417bdde8ca0bf
aamandaericson <amandaericson1@hotmail.com>
2022-12-27 13:54:16 +0000

    Add function for computer choice and display

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,9 @@ const userChoiceDisplay = document.getElementById('your-choice')
 const cpuChoiceDisplay = document.getElementById('cpu-choice')
 const resultDisplay = document.getElementById('result')
 const possibleChoices = document.getElementsByClassName('button')
+
 let userChoice;
+let cpuChoice;
 
  /*Wait for the DOM to finnish loading before start. Get the button elements by class name and add event listeners to them.
  Display user choice */
@@ -16,9 +18,32 @@ function init() {
         possibleChoice.addEventListener("click", function() {
             userChoice = this.getAttribute("data-type"); 
             userChoiceDisplay.innerHTML = userChoice;
+            generateCpuChoice()
             });
         };
     };
+
+    /**
+     * Generate the computer choice and 
+     */
+function generateCpuChoice() {
+
+    const randomNumber = Math.floor(Math.random() * 3) + 1; 
+
+    if (randomNumber === 1) {
+        cpuChoice = 'rock'
+    }
+
+    if (randomNumber === 2) {
+        cpuChoice = 'paper'
+    }
+
+    if (randomNumber === 3) {
+        cpuChoice = 'scissors'
+    }
+    cpuChoiceDisplay.innerHTML = cpuChoice;
+
+}
 
 
 
