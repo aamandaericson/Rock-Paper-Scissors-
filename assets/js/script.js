e393ee4cd9f032222dff2a5bff0653a710df23f7
aamandaericson <amandaericson1@hotmail.com>
2022-12-27 13:43:53 +0000

    Add variables, start initial function for DOM load

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,14 @@
-console.log("hello")
+//variables 
+
+const userChoiceDisplay = document.getElementById('your-choice')
+const cpuChoiceDisplay = document.getElementById('cpu-choice')
+const resultDisplay = document.getElementById('result')
+const possibleChoices = document.getElementsByClassName('button')
+let userChoice;
+
+
+document.addEventListener("DOMContentLoaded", init());
+
+
+
+
